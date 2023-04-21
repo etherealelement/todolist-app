affbeed1c231d136c48d7ac9343dfaefd0698dbf
ethereal element <vitalik.safonov@list.ru>
2023-04-21 08:00:08 +0300

    create: store

--- src/view/App/index.tsx
+++ src/view/App/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styles from "./index.module.scss"
 
+import { useToDoStore } from "../../data/stores/useToDoStore";
+
 export const App: React.FC = () => {
+	const [
+		tasks,
+		createTask,
+		updateTask,
+		removeTask
+	] = useToDoStore(state => [
+			state.tasks,
+			state.createTask,
+			state.updateTask,
+			state.removeTask,
+	]);
+
+	useEffect(() => {
+		createTask("fucking slaves")
+	}, [])
+	
 	return (
 		<>
 		<article className={styles.article}>
